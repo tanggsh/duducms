8119aa5acfa305fa700e4289c3bd3529411b4f83
SAdmin <SAdmin@DearJie>
2014-07-03 23:29:09 +0800

    新框架提交

---
+++ htmls/Js/MenuData.js
@@ -0,0 +1,72 @@
+var Nav = [
+	{
+		'name':"管理首页",
+		'key_name':"Index",
+		'data':[
+		{url:'main.html',title:"管理首页"},
+		{url:'addcate.html',title:"修改密码"},
+		{url:'http://www.qq.com',title:"安全退出"}
+		]
+	},
+	{
+		'name':"应用管理",
+		'key_name':"App",
+		'data':[
+			{url:'?app=admin&mod=news',title:"新闻频道"},
+			{url:'?app=admin&mod=article',title:"文章频道"},
+			{url:'?app=admin&mod=article',title:"图片频道"},
+			{url:'?app=admin&mod=topic',title:"专题频道"},
+			{url:'?app=admin&mod=info',title:"信息频道"},
+			{url:'?app=admin&mod=product',title:"产品频道"},
+			{url:'?app=admin&mod=case',title:"案例频道"}
+		]
+	},
+	{
+		'name':"用户管理",
+		'key_name':"User",
+		'data':[
+			{url:'?app=admin&mod=user',title:"用户列表"},
+			{url:'?app=admin&mod=space',title:"用户空间"}
+		]
+	},
+	{
+		'name':"企业圈",
+		'key_name':"group",
+		'data':[
+			{url:'?app=admin&mod=group',title:"企业圈"},
+			{url:'?app=admin&mod=group',title:"认证机制"}
+		]
+	},
+	{
+		'name':"附件管理",
+		'key_name':"Attach",
+		'data':[
+			{url:'?app=admin&mod=file',title:"文档模块"}
+			
+		]
+	},
+	{
+		'name':"系统管理",
+		'key_name':"System",
+		'data':[
+			{url:'?app=admin&mod=system',title:"系统设置"}, //基本设置 附件配置 SEO设置
+			{url:'?app=admin&mod=app',title:"APP管理"},
+			{url:'?app=admin&mod=email',title:"邮件设置"},
+			{url:'?app=admin&mod=menu',title:"导航设置"},
+			{url:'?app=admin&mod=database',title:"数据库管理"}
+			
+			
+		]
+	},
+	{
+		'name':"扩展管理",
+		'key_name':"Attach",
+		'data':[
+			{url:'?app=admin&mod=widget',title:"碎片管理"},
+			{url:'?app=admin&mod=ads',title:"广告管理"}
+		]
+	}
+];
+
+
+
